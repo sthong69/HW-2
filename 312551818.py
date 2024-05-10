5315d3646ba9b73f597fc8a74fc506e6e0db2fff
Sylvain <sylvain.thong@imt-atlantique.net>
2024-05-10 20:40:30 +0800

    comments

--- 312551818.py
+++ 312551818.py
@@ -23,32 +23,33 @@ from transformers import pipeline
 from transformers import TrainingArguments, Trainer
 import evaluate
 
+# Converts JSON file to a pandas' dataframe
 def convert_json_dataframe(json):
     res = {}
     for i in range(len(json)):
         res[i] = json[i]
     return pd.DataFrame.from_dict(res, orient='index', columns=['rating', 'title', 'text', 'helpful_vote', 'verified_purchase'])
 
+# Removes HTML tags from the input string
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, ' ', text)
 
-def decontracted(phrase):
-    # specific
-    phrase = re.sub(r"won\'t", "will not", phrase)
-    phrase = re.sub(r"can\'t", "can not", phrase)
+# Replaces contracted forms to normal forms to address tokenization issues that could occur
+def decontracted(text):
+    text = re.sub(r"won\'t", "will not", text)
+    text = re.sub(r"can\'t", "can not", text)
+    text = re.sub(r"n\'t", " not", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'s", " is", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'t", " not", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'m", " am", text)
+    return text
 
-    # general
-    phrase = re.sub(r"n\'t", " not", phrase)
-    phrase = re.sub(r"\'re", " are", phrase)
-    phrase = re.sub(r"\'s", " is", phrase)
-    phrase = re.sub(r"\'d", " would", phrase)
-    phrase = re.sub(r"\'ll", " will", phrase)
-    phrase = re.sub(r"\'t", " not", phrase)
-    phrase = re.sub(r"\'ve", " have", phrase)
-    phrase = re.sub(r"\'m", " am", phrase)
-    return phrase
-
+# Converts NLTK part-of-speech's tags to wordnet's equivalent
 def pos_tagger(nltk_tag):
     if nltk_tag.startswith('J'):
         return wordnet.ADJ
@@ -61,6 +62,7 @@ def pos_tagger(nltk_tag):
     else:          
         return None
     
+# Lemmatizes a sentence based on given associated part-of-speech's tags to prevent wrong lemmatization
 def lemmatize(list):
     lemmatizer = nltk.stem.WordNetLemmatizer()
     res = []
@@ -70,15 +72,16 @@ def lemmatize(list):
         else:
             res.append(lemmatizer.lemmatize(item[0], item[1]))
     return res
-    
+
+# All the methods used to preprocess the text input are called in the NLTK pipeline
 def nltk_pipeline(string):
-    #Remove HTML tags
+    # Remove HTML tags
     res = remove_html_tags(string)
     # Remove contracted form
     res = decontracted(res)
     # Tokenize
     res = nltk.word_tokenize(res)
-    # POS
+    # POS 
     res = nltk.pos_tag(res)
     # Convert POS to WordNet
     res = [(_[0],pos_tagger(_[1])) for _ in res]
@@ -101,6 +104,7 @@ def nltk_pipeline(string):
 
     return alt_res
 
+# Class defined and used to convert our features' list and labels to the transformers' dataset format with appropriate methods
 class ReviewDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
         self.encodings = encodings
@@ -114,6 +118,7 @@ class ReviewDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
+# Converts the string output given by our pre-trained model to a int format
 def extract_prediction_results(pred):
     res = []
     for i in range(len(pred)):
@@ -133,6 +138,7 @@ def extract_prediction_results(pred):
         res.append(grade)
     return res
 
+# Formats and exports the answers to a CSV file 
 def format_export_answers(answers, list_features):
     res = input("Do you want to output the answers (Y/N)")
     if res == "N":
@@ -155,15 +161,18 @@ def format_export_answers(answers, list_features):
     else:
         format_export_answers(answers, list_features)
 
+# All the methods used for training/fine-tuning a model are defined in the following method
 def train_model(selected_features, df_train_input, limit, tokenizer, model, model_name, from_checkpoint):
+    # Extract the wanted features from the dataset
     df_train = df_train_input[selected_features]
-    """
-    print("Shuffling training dataset...")
-    df_train = shuffle(df_train)
-    print("Finished shuffling training dataset!")
-    """
     
-    # CUT FOR TEST PURPOSE
+    # If the limit is inferior to the dataset's size, we shuffle the training dataset
+    if limit != 35000:
+        print("Limit inferior to 35000 : shuffling training dataset...")
+        df_train = shuffle(df_train)
+        print("Finished shuffling training dataset!")
+    
+    # Cuts the dataset to the specified size limit : used to reduce computation time when needed
     df_train = df_train.iloc[:limit]
 
     print("Starting NLTK pipeline on training dataset...")
@@ -171,6 +180,7 @@ def train_model(selected_features, df_train_input, limit, tokenizer, model, mode
     df_train['title'] = df_train['title'].apply(nltk_pipeline)    
     print("Finished NLTK pipeline on training dataset!")
 
+    # Splitting the dataset in order to be able to evaluate our model during training
     train_dataframe, validation_dataframe = train_test_split(df_train, test_size=0.33, random_state=42)
     X_train_title, X_train_text, y_train = train_dataframe['title'].to_list(),train_dataframe['text'].to_list(), train_dataframe['rating'].astype(int).to_list()
     X_val_title, X_val_text, y_val = validation_dataframe['title'].to_list(),validation_dataframe['text'].to_list(), validation_dataframe['rating'].astype(int).to_list()
@@ -179,25 +189,29 @@ def train_model(selected_features, df_train_input, limit, tokenizer, model, mode
     y_train = [grade-1 for grade in y_train]
     y_val = [grade-1 for grade in y_val]
 
+    # Appends our text inputs with a "[SEP]" separator for the transformers' tokenizer later
     X_train = [X_train_title[i] + "[SEP] " + X_train_text[i] for i in range(len(X_train_title))]
     X_val = [X_val_title[i] + "[SEP] " + X_val_text[i] for i in range(len(X_val_title))]
 
-    # Encode features
+    # Encodes features with the given tokenizer, pads and truncates the content to maximum length for the model's input
     X_train_encoded = tokenizer(X_train, padding="max_length", truncation=True)
     X_val_encoded = tokenizer(X_val, padding="max_length", truncation=True)
 
-    # Convert to transformers datasets
+    # Convert to transformers' compatible datasets
     train_revdataset = ReviewDataset(X_train_encoded, y_train)
     val_revdataset = ReviewDataset(X_val_encoded, y_val)
 
+    # Defines the training's arguments
     training_args = TrainingArguments(output_dir=model_name, logging_dir='logs', evaluation_strategy="epoch", num_train_epochs=11)
+    
+    # Defines our way to compute our model's accuracy during training 
     accuracy = evaluate.load("accuracy")
-
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
         predictions = np.argmax(predictions, axis=1)
         return accuracy.compute(predictions=predictions, references=labels)
     
+    # Defines our tranformers' trainer
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -212,7 +226,9 @@ def train_model(selected_features, df_train_input, limit, tokenizer, model, mode
     trainer.save_model(output_dir="trained-models/"+model_name)
     print("Model saved!")
 
+# All the methods used for making predictions on the test dataset with a given model are defined in the following method
 def make_predictions(selected_features, df_test_input, tokenizer, model):
+    # Extract the wanted features from the dataset
     df_test = df_test_input[selected_features]
 
     # CUT FOR TEST PURPOSE
@@ -261,43 +277,7 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-
-
-
-
-
-
-
-
-
-#X_test_encoded = tokenizer(X_test, padding="max_length", truncation=True)
-
-
-
-
-#test_dataset = IMDbDataset(test_encodings, test_labels)
-
-
-
-
-# learning_rate=2e-05, per_gpu_train_batch_size=16, per_gpu_eval_batch_size=16, per_device_train_batch_size=1, gradient_accumulation_steps=4,gradient_checkpointing=True
-
-
-
-
-
-
-
-"""
-for result in predictions:
-    print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
-"""
-
-
-# Check spread of ratings
-
+# Checks spread of ratings
 def check_ratings(dataframe):
     res = dataframe.rating.value_counts()
     print(res)
